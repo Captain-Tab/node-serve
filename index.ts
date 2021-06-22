e41b486d4a8d5f02c82da61fc8b5486e460a7c01
captain-tab <d491405879@gmail.com>
2021-06-22 23:00:19 +0800

    solve the request query

--- index.ts
+++ index.ts
@@ -2,13 +2,22 @@ import * as http from 'http'
 import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as path from 'path'
+import * as url from 'url'
+const { parse: parseQuery } = require('querystring');
 
 const server = http.createServer()
 const publicDir = path.resolve(__dirname, 'public')
+const serverOrigin = 'http://localhost:9999';
+
 
 server.on('request', (request: IncomingMessage, response: ServerResponse)=> {
-    const { method, url, headers} = request
-    switch (url) {
+    const { method, url: urlPath, headers} = request
+    const url = new URL(request.url, serverOrigin);
+    const {pathname} = url
+    // Parse the URL query. The leading '?' has to be removed before this.
+    const query = parseQuery(url.search.substr(1));
+    console.log('url',pathname)
+    switch (pathname) {
         case '/index.html':
             fs.readFile(path.resolve(publicDir, 'index.html'), (error, data)=> {
                 if (error) throw error
@@ -29,6 +38,9 @@ server.on('request', (request: IncomingMessage, response: ServerResponse)=> {
                 response.end(data.toString())
             })
             break;
+        default:
+            response.statusCode = 404
+            response.end()
     }
 })
 
